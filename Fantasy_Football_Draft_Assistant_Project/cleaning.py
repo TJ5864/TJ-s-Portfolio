fb4991276be04f48488f3c556d5526b4cd88d9b9
TJ5864 <137420132+TJ5864@users.noreply.github.com>
2025-07-22 08:58:30 -0400

    Update cleaning.py

--- Fantasy_Football_Draft_Assistant_Project/cleaning.py
+++ Fantasy_Football_Draft_Assistant_Project/cleaning.py
@@ -3,10 +3,10 @@ import pandas as pd
 
 
 try:
-    df = pd.read_csv("Data/all_positions_2024_projections.csv", header=[0, 1])
+    df = pd.read_csv("all_positions_2024_projections.csv", header=[0, 1])
     df.columns = [' '.join(col).strip() for col in df.columns]
 except:
-    df = pd.read_csv("Data/all_positions_2024_projections.csv")
+    df = pd.read_csv("all_positions_2024_projections.csv")
     df.columns = df.columns.str.strip()
 
 # Optional: Fill NaNs with 0
@@ -20,7 +20,7 @@ df.rename(columns={
     'Unnamed: 0_level_0 Team': 'Team',
 }, inplace=True)
 # Save cleaned version
-df.to_csv("Data/cleaned_player_projections.csv", index=False)
+df.to_csv("cleaned_player_projections.csv", index=False)
 
 # Print column names to verify
 print(df.columns.tolist())
